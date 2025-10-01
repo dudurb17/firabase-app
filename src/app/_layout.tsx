55c0ce8571a3c5988f173aab408f89d1782d5181
Eduardo <eduardo.robettibedin@gmail.com>
2025-10-01 17:35:56 -0300

    Refactor RootLayout component to remove SafeAreaView for streamlined navigation structure. Maintain existing screen options for improved user experience.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,22 @@
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          animation: "slide_from_right",
-          animationDuration: 300,
-          gestureEnabled: true,
-          gestureDirection: "horizontal",
-          freezeOnBlur: true,
-        }}
-      >
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen
-          name="(auth)"
-          options={{ animation: "slide_from_bottom" }}
-        />
-      </Stack>
-    </SafeAreaView>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+        animationDuration: 300,
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+        freezeOnBlur: true,
+      }}
+    >
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(auth)"
+        options={{ animation: "slide_from_bottom" }}
+      />
+    </Stack>
   );
 }
